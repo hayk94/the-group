331539fbc173a8dc001b6fcad68ddb84cea9e72c
Hayk <hayk_safaryan@epam.com>
2020-10-01 19:44:16 +0400

    add react-router

--- src/App.js
+++ src/App.js
@@ -3,11 +3,26 @@ import theme from "./theme";
 import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { LOGIN_PATH, REGISTER_PATH } from "./routes";
+import LoginView from "./views/LoginView";
+import RegisterView from "./views/RegisterView";
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      Hello world
+      <Router>
+        <Switch>
+          <Route path={REGISTER_PATH}>
+            <RegisterView />
+          </Route>
+          <Route path={LOGIN_PATH}>
+            <LoginView />
+          </Route>
+          <Route path="/">Hello</Route>
+        </Switch>
+      </Router>
     </ThemeProvider>
   );
 }
